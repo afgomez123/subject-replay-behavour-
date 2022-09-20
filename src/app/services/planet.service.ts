a16f8b56246a9b783840c4e31dfb846ae7fa8486
afgomez123 <36038728+afgomez123@users.noreply.github.com>
2022-09-20 15:50:24 -0500

    1 file added

---
+++ src/app/services/planet.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PlanetService {
+  constructor(private _http: HttpClient) {}
+
+  getValues() {
+    const url = 'https://planets-by-api-ninjas.p.rapidapi.com/v1/planets';
+    return this._http
+      .get(url, {
+        params: { name: 'Mars' },
+        headers: {
+          'X-RapidAPI-Key':
+            '7264dbb400msh036a7bb1affd8ddp1e3bc5jsn223afb140201',
+          'X-RapidAPI-Host': 'planets-by-api-ninjas.p.rapidapi.com',
+        },
+      })
+      .subscribe((planet) => console.log(planet));
+  }
+}
